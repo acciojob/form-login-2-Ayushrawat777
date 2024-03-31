dcd28b6d9b1ac76ad297191be4d437aa81274603
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-01 00:59:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+   document.getElementById("MyForm").onsubmit = volume_sphere;
+      function volume_sphere() {
+    let a=    document.getElementById("firstname").value;
+    let b=    document.getElementById("lastname").value;
+    let c=    document.getElementById("number").value;
+    let d=    document.getElementById("email").value;
+        alert(a+b+c+d)
+      }
